9254c3396df03d7c26e2c06860b11ce12894a76e
aaronshin43 <aaronshin.exe@gmail.com>
2026-02-10 21:23:14 -0500

    Adjust parameters for val/accuracy

--- framework/config/addition_1digit.py
+++ framework/config/addition_1digit.py
@@ -3,7 +3,7 @@
 # This will be good for testing simple prompt-response behavior
 
 # out_dir must match the directory where you want checkpoints saved
-out_dir = 'out/addition_2digit'
+out_dir = 'out/addition_1digit'
 eval_interval = 50 
 eval_iters = 20
 log_interval = 1 
@@ -14,26 +14,26 @@ always_save_checkpoint = False
 # Weights & Biases logging
 wandb_log = True # set to True to enable logging to wandb
 wandb_project = 'shina-addition-intermediate'
-wandb_run_name = '2digit-5000iters-50sample-greedy'
+wandb_run_name = '1digit-2000iters-greedy'
 
-dataset = 'addition_2digit'
+dataset = 'addition_1digit'
 gradient_accumulation_steps = 1
 batch_size = 32
-block_size = 128
+block_size = 64
 
 # very very small GPT model
-n_layer = 4
-n_head = 4
-n_embd = 128  # need n_embd % n_head == 0
+n_layer = 3
+n_head = 3
+n_embd = 120  # need n_embd % n_head == 0
 dropout = 0.0
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 5000
-lr_decay_iters = 5000 # make equal to max_iters usually
+max_iters = 2000
+lr_decay_iters = 2000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
-warmup_iters = 100 # 100 # not super necessary potentially
+warmup_iters = 0 # 100 # not super necessary potentially
 
 # device = 'cpu'  # run on cpu only
 compile = False # do not torch compile the model
@@ -48,4 +48,6 @@ stop_token = "\n"
 
 num_samples = 1
 max_new_tokens = 50
-seed = 1337
+seed = 1338
+
+separator="="
